9ad16176f2e06e5081f26cd0bad4327fcc45d128
GrantLicata <grant.licata@gmail.com>
2023-03-18 21:45:18 -0700

    Login screen complete

--- App.js
+++ App.js
@@ -1,36 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
 
 // Stack structure brought in using navigator.
 const Stack = createNativeStackNavigator();
 
 // Formatting applied to all screens within the app.
 const globalScreenOptions = {
-  headerStyle: {backgroundColor: "#2C6BED"},
-  headerTitleStyle: {color: "white"},
-  headerTintColor: "White",
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "Wh``ite",
 };
- 
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={globalScreenOptions}>
-        <Stack.Screen name='Login' component={ LoginScreen } />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Button, Input, Image } from '@rneui/themed';
+import { React, useState } from "react";
+import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
+import { Button, Input, Image } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
   return (
-    <View>
-      <Text>I am the login screen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }}
+        style={{
+          width: 140,
+          height: 140,
+          borderRadius: 25,
+          margin: 10,
+        }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
