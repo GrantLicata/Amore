cd596d796293adb88d954dbab1fab94e6840535a
GrantLicata <grant.licata@gmail.com>
2023-03-18 19:33:48 -0700

    React native elements prepared

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
 
+// Stack structure brought in using navigator.
+const Stack = createNativeStackNavigator();
+
+// Formatting applied to all screens within the app.
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: "#2C6BED"},
+  headerTitleStyle: {color: "white"},
+  headerTintColor: "White",
+};
+ 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Lets build Amore!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={ LoginScreen } />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button, Input, Image } from '@rneui/themed';
 
 const LoginScreen = () => {
   return (
