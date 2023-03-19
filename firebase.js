bf47bb272e413a23d9192033e2bc76f223d0bccb
GrantLicata <grant.licata@gmail.com>
2023-03-18 18:56:37 -0700

    Firebase prepared

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcFapk5s9_kxGI36MH7Ri1IbSsfqPoJZc",
+  authDomain: "amore-1a691.firebaseapp.com",
+  projectId: "amore-1a691",
+  storageBucket: "amore-1a691.appspot.com",
+  messagingSenderId: "154332204352",
+  appId: "1:154332204352:web:620a3de48f6290c504fea5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
