c2e19a608fe42a6152a296947a86c3b0dbf46499
GrantLicata <grant.licata@gmail.com>
2023-03-23 19:16:36 -0700

    Firebase initial configuration with authentication

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +12,20 @@ const firebaseConfig = {
   projectId: "amore-1a691",
   storageBucket: "amore-1a691.appspot.com",
   messagingSenderId: "154332204352",
-  appId: "1:154332204352:web:620a3de48f6290c504fea5"
+  appId: "1:154332204352:web:620a3de48f6290c504fea5",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase only as needed
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
